22bd0373da93ef4c97c6b0a99a7e71e25cfaeb0f
Ashutosh Tiwari <ashutiwa@iu.edu>
2024-04-14 12:58:11 -0400

    removing tox as dependency

--- tests/test_cython.py
+++ tests/test_cython.py
@@ -3,6 +3,9 @@ import pytest
 
 
 @pytest.fixture()
+def trie():
+    return CyTrie()
+@pytest.fixture()
 def ca_words():
     return ["cat", "car", "cartoon", "carate"]
 
@@ -17,8 +20,7 @@ def words(ta_words, ca_words):
     return ta_words + ca_words
 
 
-def test_find(words):
-    trie = CyTrie()
+def test_find(trie, words):
     trie.extend(words)
     for word in words:
         assert word in trie
@@ -26,19 +28,16 @@ def test_find(words):
     assert "rofl" not in trie
 
 
-def test_prefix_not_in_trie():
-    trie = CyTrie()
+def test_prefix_not_in_trie(trie):
     assert "ca" not in trie
 
 
-def test_len(words):
-    trie = CyTrie()
+def test_len(words, trie):
     trie.extend(words)
     assert len(trie) == len(words)
 
 
-def test_iter(words):
-    trie = CyTrie(words)
+def test_iter(words, trie):
     trie.extend(words)
     assert sorted(trie) == sorted(words)
 
@@ -51,16 +50,14 @@ def test_iter(words):
         (["cat"] * 2 + ["car"] * 3 + ["cartoon"] * 1, ["car", "cat", "cartoon"]),
     ],
 )
-def test_sorted_by_use(words, expected):
-    trie = CyTrie(words)
+def test_sorted_by_use(trie, words, expected):
     trie.extend(words)
     assert list(trie) == expected
     assert list(trie.complete("ca")) == expected
 
 
 @pytest.mark.parametrize("words", [["cat"] * 3 + ["car"] * 1 + ["cartoon"] * 2])
-def test_query_count(words):
-    trie = CyTrie(words)
+def test_query_count(trie, words):
     trie.extend(words)
     assert trie.count("cat") == 3
     assert trie.count("car") == 1
@@ -78,8 +75,7 @@ def test_query_count(words):
         (["ct", "a", "tb"], ["cat", "tabla", "table", "taboo"]),
     ],
 )
-def test_ambiguous_completion(input_groups, expected):
-    trie = CyTrie()
+def test_ambiguous_completion(trie, input_groups, expected):
     trie.extend(expected)
     assert sorted(trie.complete(input_groups)) == sorted(expected)
 
@@ -88,7 +84,6 @@ def test_ambiguous_completion(input_groups, expected):
     ("words", "input_groups", "expected"),
     [(["cat"] * 3 + ["car"], ["cv", "as", "rt"], ["cat", "car"])],
 )
-def test_ambiguous_completion_sorted_by_use(words, input_groups, expected):
-    trie = CyTrie()
+def test_ambiguous_completion_sorted_by_use(trie, words, input_groups, expected):
     trie.extend(words)
     assert list(trie.complete(input_groups)) == expected
