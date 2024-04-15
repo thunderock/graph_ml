63e3009f369d8ea278400634069c31374653b86b
Ashutosh Tiwari <ashutiwa@iu.edu>
2024-04-14 21:05:11 -0400

    removed lightening as dependency

--- graph_ml/trainers/trainer.py
+++ graph_ml/trainers/trainer.py
@@ -1,18 +1,15 @@
 # Author: Ashutosh Tiwari
 # Date: 4/14/24
 # Project: GraphML
-import pytorch_lightning as pl
 from ..utils import config
 
 
 class Trainer(object):
     """
-    takes a pl.LightningModule and trains it using the training paradigm
+    takes a torch module and trains it using the training paradigm
     can be Contrastive learning, supervised learning or any other learning paradigm
     """
 
     def __init__(self, model, **kwargs):
         self.model = model
-        self.trainer = pl.Trainer(**kwargs)
-        pl.seed_everything(config.SEED)
         self.tuning = config.TUNING
