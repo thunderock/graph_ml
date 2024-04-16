d565a9ee5fa6d84176a22329d238305b3abbb2d1
Ashutosh Tiwari <findashutoshtiwari@gmail.com>
2024-04-16 16:16:39 -0400

    started adding smaplers

--- graph_ml/transformations/samplers.py
+++ graph_ml/transformations/samplers.py
@@ -2,6 +2,10 @@
 # Date: 3/19/24
 # Project: GraphML
 
+import numpy as np
+import torch
+from ..utils import torch_utils, utils, config
+
 
 class Sampler(object):
     """
@@ -10,10 +14,42 @@ class Sampler(object):
     May be should be changed in future, fit to be allowed to called multiple times.
     """
 
-    def __init__(self):
-        pass
-
     def fit(
-        self,
+        self, A
     ):
-        pass
+        raise NotImplementedError
+
+    def sample(self, centers, contexts, padding_mask=None):
+        raise NotImplementedError
+
+
+class SbmNodeSampler(Sampler):
+    def __init__(self, window_length=10, group_membership=None, dcsbm=True):
+        """
+        Parameters
+        ----------
+        window_length : int
+            Number of nodes to sample in the context window
+        group_membership : np.ndarray
+            Group membership of each node
+        dcsbm : bool
+            sampling to take degree corrected sbm or not
+        """
+        self.window_length = window_length
+        self.dcsbm = dcsbm
+        self.group_membership = group_membership
+
+    def fit(self, A):
+        """
+        Fit the sampler to the adjacency matrix
+        Parameters
+        ----------
+        A : np.ndarray
+            Adjacency matrix
+        """
+        self.n_nodes = A.shape[0]
+        if self.group_membership is None:
+            self.group_membership = np.zeros(self.n_nodes, dtype=np.int32)
+        self.node2group = utils.to_member_matrix(self.group_membership)
+
+        indeg = A.sum(axis=0)
