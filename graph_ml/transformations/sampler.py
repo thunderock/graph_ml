2731f95be06541cba7ba26f449a6c179c6eed74e
Ashutosh Tiwari <findashutoshtiwari@gmail.com>
2024-06-28 23:48:26 -0400

    fixed build

--- graph_ml/transformations/sampler.py
+++ graph_ml/transformations/sampler.py
@@ -16,22 +16,32 @@ class Sampler(object):
             self.number_target = number_target
         return self.number_target
 
-    def __init__(self, window_length: int = 10, number_target: Optional[int] = None, seed: Optional[int] = None, degree_agnostic: Boolen) -> None:
+    def __init__(
+        self,
+        window_length: int = 10,
+        number_target: Optional[int] = None,
+        seed: Optional[int] = None,
+        degree_agnostic: bool = False,
+    ):
         """
         Parameters
         ----------
         window_length : int
             Number of nodes to sample in the context window
         number_target : int
-               Number of target nodes or edges to sample, can be none because fit can take centers
+            Number of target nodes or edges to sample,
+            can be none because fit can take centers
         seed : int
             Seed for random number generator
         """
         self.window_length = window_length
         self.number_target = number_target
-        self.seed = utils.get_formatted_environ_variable("SEED", int, 42) if seed is None else seed
+        self.seed = (
+            utils.get_formatted_environ_variable("SEED", int, 42)
+            if seed is None
+            else seed
+        )
         self._set_seed()
-
 
     def num_nodes(self, A):
         return A.shape[0]
@@ -41,6 +51,8 @@ class Sampler(object):
 
     def _generate_centers(self, A):
         return np.random.choice(self.num_nodes(A), self.number_target, replace=False)
-    def sample(self, centers: Optional[np.ndarray], padding_mask: int = 0) -> np.ndarray:
-        raise NotImplementedError
 
+    def sample(
+        self, centers: Optional[np.ndarray], padding_mask: int = 0
+    ) -> np.ndarray:
+        raise NotImplementedError
